48160274813589a46ab5b50c5583821e39a47444
Mehak Vohra <watthemehak@gmail.com>
2019-01-04 21:57:55 -0800

    updated for subdomain

--- src/utils/Auth/hostedConfig.js
+++ src/utils/Auth/hostedConfig.js
@@ -1,6 +1,6 @@
 export default {
   urls: {
-    login: "https://client-cms-contentful.herokuapp.com/login",
-    redirectUri: "https://client-cms-contentful.herokuapp.com/auth-handler"
+    login: "http://app.jamochamedia.com/login",
+    redirectUri: "http://app.jamochamedia.com/auth-handler"
   }
 };
