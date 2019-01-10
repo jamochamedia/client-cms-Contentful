0190be151fb3327bca3802b3f3ed480053965730
Mehak Vohra <watthemehak@gmail.com>
2019-01-09 16:49:04 -0800

    removed node_modules

--- client/src/utils/Auth/hostedConfig.js
+++
@@ -1,6 +0,0 @@
-export default {
-  urls: {
-    login: "http://app.jamochamedia.com/login",
-    redirectUri: "http://app.jamochamedia.com/auth-handler"
-  }
-};
--- client/src/utils/Auth/localConfig.js
+++
@@ -1,6 +0,0 @@
-export default {
-  urls: {
-    login: "http://localhost:3000/login",
-    redirectUri: "http://localhost:3000/auth-handler"
-  }
-};
