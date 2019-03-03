5005dad914389356df15f3be29df900ec79fa0a0
Mehak Vohra <watthemehak@gmail.com>
2019-03-02 17:15:17 -0800

    removed header

--- server/src/server.js
+++ server/src/server.js
@@ -12,37 +12,40 @@ const contentfulClient = contentful.createClient({
     "995a6dca6f0f6cd6e2fdb805d631c96af1cda58513b55ba824668d8fdfa18966"
 });
 
-const auth0Client = {
-  audience: "<tqieceTsEIowUTJGkFw3QzYXVxn218m1>",
-  issuer: `https://jamocha.auth0.com/`,
-  algorithms: ["RS256"]
-};
+// const auth0Client = {
+//   audience: "<tqieceTsEIowUTJGkFw3QzYXVxn218m1>",
+//   issuer: `https://jamocha.auth0.com/`,
+//   algorithms: ["RS256"]
+// };
 
-const userClient = jwksClient({
-  jwksUri: `https://jamocha.auth0.com/.well-known/jwks.json`
-});
+// const userClient = jwksClient({
+//   jwksUri: `https://jamocha.auth0.com/.well-known/jwks.json`
+// });
 
-function getKey(header, cb) {
-  userClient.getSigningKey(header.kid, function(err, key) {
-    var signingKey = key.publicKey || key.rsaPublicKey;
-    cb(null, signingKey);
-  });
-}
+// function getKey(header, cb) {
+//   userClient.getSigningKey(header.kid, function(err, key) {
+//     var signingKey = key.publicKey || key.rsaPublicKey;
+//     cb(null, signingKey);
+//   });
+// }
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: ({ req }) => {
-    const token = req.headers.authorization;
-    const user = new Promise((resolve, reject) => {
-      jwt.verify(token, getKey, auth0Client, (err, decoded) => {
-        if (err) {
-          return reject(err);
-        }
-        resolve(decoded.email);
-      });
-    });
-    return { user, contentfulClient };
+    // const token = req.headers.authorization;
+    // const user = new Promise((resolve, reject) => {
+    //   jwt.verify(token, getKey, auth0Client, (err, decoded) => {
+    //     if (err) {
+    //       return reject(err);
+    //     }
+    //     resolve(decoded.email);
+    //   });
+    // });
+    return {
+      // user,
+      contentfulClient
+    };
   }
 });
 
